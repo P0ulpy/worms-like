8efac987dd15ca476963ecf479f0f2215b0c7bf9
mbocciarelli <bocciarellimathieu@orange.fr>
2023-01-22 03:16:10 +0100

    Update Logger to varidatic function

--- Engine/Engine/Core/Logger/Logger.hpp
+++ Engine/Engine/Core/Logger/Logger.hpp
@@ -29,13 +29,23 @@ namespace Engine
     {
     public:
         static std::list<LogEntry> messages;
-        static void Log(const std::string_view& message);
-        static void Warn(const std::string_view& message);
-        static void Err(const std::string_view& message);
+        template<typename ...Types>
+        static void Log(const Types&... message);
+
+        template<typename ...Types>
+        static void Warn(const Types&... message);
+
+        template<typename ...Types>
+        static void Err(const Types&... message);
 
         static void SetThreadLabel(const std::string_view& label);
         static void SetThreadLabel(const std::string_view& label, std::thread::id threadID);
     private:
+        static LogEntry ConstructEntry(LogType type, const std::string& header);
+
+        template<typename FirstMessage, typename ...Messages>
+        static void BuildMessage(const FirstMessage& first, const Messages&... messages, std::string& base);
+
         static std::mutex _mutex;
 
         static std::unordered_map<std::thread::id, std::string> _threadsLabels;
