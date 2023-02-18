5e8af5ff96fe0e51e072e325483f144337a38e7f
mbocciarelli <bocciarellimathieu@orange.fr>
2023-02-18 15:33:00 +0100

    Correction AssetLoader

--- Engine/Engine/AssetLoader/AssetLoader.cpp
+++ Engine/Engine/AssetLoader/AssetLoader.cpp
@@ -5,4 +5,28 @@
 #include "AssetLoader.hpp"
 
 namespace Engine
-{} // Engine
+{
+    template<>
+    sf::Texture* AssetLoader<sf::Texture>::StaticLoadAsset(const std::string& path)
+    {
+        sf::Texture texture;
+        if(!texture.loadFromFile(path))
+            return nullptr;
+
+        s_paths[path] = texture;
+
+        return &s_paths[path];
+    }
+
+    template<>
+    sf::Font* AssetLoader<sf::Font>::StaticLoadAsset(const std::string& path)
+    {
+        sf::Font font;
+        if(!font.loadFromFile(path))
+            return nullptr;
+
+        s_paths[path] = font;
+
+        return &s_paths[path];
+    }
+} // Engine
--- Engine/Engine/AssetLoader/AssetLoader.hpp
+++ Engine/Engine/AssetLoader/AssetLoader.hpp
@@ -7,6 +7,7 @@
 
 #include <SFML/Graphics/Texture.hpp>
 #include <unordered_map>
+#include "SFML/Graphics/Font.hpp"
 
 namespace Engine {
 
@@ -40,15 +41,10 @@ namespace Engine {
     }
 
     template<>
-    sf::Texture* AssetLoader<sf::Texture>::StaticLoadAsset(const std::string& path)
-    {
-        sf::Texture texture;
-        texture.loadFromFile(path);
+    sf::Texture* AssetLoader<sf::Texture>::StaticLoadAsset(const std::string& path);
 
-        s_paths[path] = texture;
-
-        return &s_paths[path];
-    }
+    template<>
+    sf::Font* AssetLoader<sf::Font>::StaticLoadAsset(const std::string& path);
 
 } // Engine
 
