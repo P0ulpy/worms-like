0f5eb95cfcc0e798a2c762030b0b01218ecc86e2
NorthBlue333 <north333@free.fr>
2023-03-03 10:38:58 +0100

    Fix frag grenade for demo

--- Game/Game/Prefabs/GrenadeFragPrefab.h
+++ Game/Game/Prefabs/GrenadeFragPrefab.h
@@ -10,16 +10,14 @@
 class GrenadeFragPrefab : public Engine::EntityPrefab<Game::Weapons::GrenadeFragmentation>
 {
 public:
-    void Init(Engine::Entity& entity) override;
+    void Init(Engine::Entity& entity) override
+    {
+        auto grenadeTransform = entity.GetComponent<Engine::Components::Transform>();
+        auto grenade = entity.AddComponent<Game::Weapons::GrenadeFragmentation>();
+
+        const auto& Window = Engine::EngineApplication::Get()->GetWindow();
+        auto MousePixelPosition = sf::Mouse::getPosition();
+        auto MousePos = Window.mapPixelToCoords(MousePixelPosition);
+        grenadeTransform->Pos = Maths::Point2D<double>(static_cast<double>(MousePos.x), static_cast<double>(MousePos.y));
+    }
 };
-
-void GrenadeFragPrefab::Init(Engine::Entity &entity)
-{
-    auto grenade = entity.AddComponent<Game::Weapons::GrenadeFragmentation>();
-    auto grenadeTransform = entity.GetComponent<Engine::Components::Transform>();
-
-    const auto& Window = Engine::EngineApplication::Get()->GetWindow();
-    auto MousePixelPosition = sf::Mouse::getPosition();
-    auto MousePos = Window.mapPixelToCoords(MousePixelPosition);
-    grenadeTransform->Pos = Maths::Point2D<double>(static_cast<double>(MousePos.x), static_cast<double>(MousePos.y));
-}
